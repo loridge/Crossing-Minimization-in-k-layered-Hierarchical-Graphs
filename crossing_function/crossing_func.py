4d9b36e0aed59f0bcb21901e7403c661cbc10cba
ZandrewG <zcgarais@up.edu.ph>
2025-02-15 11:57:19 +0800

    chore: docstring for crossing function

--- crossing_function/crossing_func.py
+++ crossing_function/crossing_func.py
@@ -2,18 +2,27 @@ from crossing_function.crossing_utils import node_neighbors, u_prime_neighbor_fi
 
 # Crossing function or Objective Function
 def cross_count(fixed_layer: list[str], free_layer: list[str], pos_data: dict, edges: list, layered_pos_data=None) -> int:
-    """_summary_
+    """
+    Calculate the number of edge crossings between two layers in a bipartite graph.
 
-    Args:
-        fixed_layer (list[str]): _description_
-        free_layer (list[str]): _description_
-        pos_data (dict): _description_
-        edges (list): _description_
-        layered_pos_data (_type_, optional): _description_. Defaults to None.
+    This function counts all of the crossings present in a bipartite graph. The function takes two lists of node labels representing 
+    two layers of the graph and the positional data of the nodes in the Cartesian plane. U is free, L is fixed. 
+    node u belongs to free_layer. node v belongs to fixed_layer.
+    
+    Args:        
+        fixed_layer (list[str]): List of node labels in the fixed layer.
+        free_layer (list[str]): List of node labels in the free layer.
+        pos_data (dict): Dictionary containing the positional data of the nodes.
+                         The keys are node labels, and the values are (x, y) tuples
+                         representing the positions of the nodes.
+        edges (list): List of edges in the graph.
+        layered_pos_data (dict, optional): Dictionary containing the layered positional data of the nodes.
+                                           Defaults to None.
 
     Returns:
-        int: _description_
+        int: The total number of edge crossings between the two layers.
     """
+    # Reference: https://studenttheses.uu.nl/bitstream/handle/20.500.12932/46720/final_Bachelor_Thesis_Mathematics.pdf?sequence=1&isAllowed=y
     crossing_total = 0
     
     for u_node in free_layer:
