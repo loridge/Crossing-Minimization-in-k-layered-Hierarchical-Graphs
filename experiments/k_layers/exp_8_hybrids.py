de93e315d8653c3e4eba7217b478b2bd516ea6dc
ZandrewG <zcgarais@up.edu.ph>
2025-04-29 23:02:07 +0800

    chore:test

--- experiments/k_layers/exp_8_hybrids.py
+++ experiments/k_layers/exp_8_hybrids.py
@@ -424,7 +424,7 @@ if __name__ == "__main__":
     bary_sift = BarySiftingCutoffHybrid(layers, edges, l_cutoff=2, parsed_layer_edge_data=parsed_data, comment_out=0)
     barysift_reordered = bary_sift.execute()
     barysift_reordered_count = total_crossing_count_k_layer(barysift_reordered, edges)
-    print(f"BarycenterSifting {barysift_reordered_count}")
+    print(f"Uncommented BarycenterSifting {barysift_reordered_count}")
     
     #commented barysift
     bary_sifter = BarySiftingCutoffHybrid(layers, edges, l_cutoff=2, parsed_layer_edge_data=parsed_data, comment_out=1)
@@ -432,11 +432,25 @@ if __name__ == "__main__":
     barysift_reordered_counter = total_crossing_count_k_layer(barysift_reordereder, edges)
     print(f"BarycenterSifting {barysift_reordered_counter}")
 
+    siftbary = SiftingBaryCutoffHybrid(layers, edges, l_cutoff=2, parsed_layer_edge_data=parsed_data, comment_out=1)
+    siftbary_reorder = siftbary.execute()
+    siftbary_reorder_counter = total_crossing_count_k_layer(siftbary_reorder, edges)
+    print(f"SiftBary {siftbary_reorder_counter}")
+    
     permusifting = PermuSiftingCutoffHybrid(layers, edges, l_cutoff=2, parsed_layer_edge_data=parsed_data, comment_out=1)
     permusifting_reorderer = permusifting.execute()
     permusifting_reordered_counter = total_crossing_count_k_layer(permusifting_reorderer, edges)
     print(f"Permusifting {permusifting_reordered_counter}")
     
+    permubary = PermuBaryCutoffHybrid(layers, edges, l_cutoff=2, parsed_layer_edge_data=parsed_data, comment_out=1)
+    permubary_reorderer = permubary.execute()
+    permubary_reordered_counter = total_crossing_count_k_layer(permubary_reorderer, edges)
+    print(f"Permubary {permubary_reordered_counter}")
+    
+    purepermu = PermuBaryCutoffHybrid(layers, edges, l_cutoff=k-1, parsed_layer_edge_data=parsed_data, comment_out=1)
+    purepermu_reorderer = purepermu.execute()
+    purepermu_reordered_counter = total_crossing_count_k_layer(purepermu_reorderer, edges)
+    print(f"Pure Permu {purepermu_reordered_counter}")
     ########## ANIMATION 
     ### may bug, the cut-off layer just oscillates kapag post-cutoff phase na
     ### new update, added the comment_out for this
