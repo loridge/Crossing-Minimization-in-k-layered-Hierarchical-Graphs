ce9f4e164f2408af224f77458992c44167d39dfc
ZandrewG <zcgarais@up.edu.ph>
2025-05-22 22:29:46 +0800

    chore: adjust num samples

--- experiments/2_layers/exp3_parallel_9.py
+++ experiments/2_layers/exp3_parallel_9.py
@@ -387,7 +387,7 @@ n2 = n2_values[0]
 p_values = [0.1, 0.2, 0.3, 0.4,0.5,0.6,0.7,0.8,0.9]  # Edge probabilities
 # p_values=[0.1,0.2,0.3,0.4]
 # p_values = [0.1]
-num_samples = 10
+num_samples = 20
 results=[]
 
 def run_experiment(n1, n2, p, num_samples):
